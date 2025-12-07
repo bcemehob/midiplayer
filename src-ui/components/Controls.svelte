b5d756ca50ab2d626bd9d7587ae1b79143bfb0eb
Vadim Semenov <bagum.cemehob@gmail.com>
2025-12-07 22:39:16 +0200

    add keyboard controls (space, shft+space for pause) for playback

--- src-ui/components/Controls.svelte
+++ src-ui/components/Controls.svelte
@@ -1,5 +1,6 @@
 <script>
   // @ts-nocheck
+  import { onMount, onDestroy } from "svelte"
   import { goToTick } from "../helpers/playback"
   import {
     isPlaybackStopped,
@@ -22,6 +23,16 @@
     audioEl.play()
   }
 
+  function toggleStartOnSpace(event) {
+    if (event.code === "Space") {
+      event.preventDefault()
+      if ($isPlaybackStopped) start()
+      else if (event.shiftKey) {
+        pause()
+      } else stop()
+    }
+  }
+
   async function stop() {
     await fetch("/api/stop")
     stopPlaybackAndSetCursor()
@@ -31,7 +42,6 @@
     await fetch("/api/pause")
     latestStartTick.set($midiEvent.tick)
     stopPlaybackAndSetCursor()
-
   }
 
   function handleEnded() {
@@ -48,18 +58,34 @@
     audioEl.pause()
     goToTick($latestStartTick)
   }
+  onMount(() => {
+    window.addEventListener("keydown", toggleStartOnSpace)
+  })
+
+  onDestroy(() => {
+    window.removeEventListener("keydown", toggleStartOnSpace)
+  })
 </script>
 
 <div>
   <div>File name: {fileName}</div>
 
   {#if $isPlaybackStopped}
-    <button class="sym" on:click={start} disabled={!fileName}>▶</button>
+    <button
+      class="sym"
+      on:click={start}
+      on:keydown={toggleStartOnSpace}
+      disabled={!fileName}>▶</button
+    >
   {:else}
     <button class="sym" on:click={pause} disabled={!fileName}>⏸</button>
   {/if}
-  <button class="sym" on:click={stop} disabled={!fileName || $isPlaybackStopped}>⏹</button>
-  <button class="sym" on:click={() => goToTick(0)} disabled={!fileName}>⏮</button>
+  <button class="sym" on:click={stop} disabled={!fileName || $isPlaybackStopped}
+    >⏹</button
+  >
+  <button class="sym" on:click={() => goToTick(0)} disabled={!fileName}
+    >⏮</button
+  >
 </div>
 <div>
   <audio bind:this={audioEl} src={audioUrl} on:ended={handleEnded}></audio>
