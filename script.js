a5bac7c376567d2774516623bbeff40b47f82b8d
Vadim Semenov <bagum.cemehob@gmail.com>
2026-01-02 16:37:28 +0200

    clean code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const { createServer } = require("./src-backend/server");
+const { createServer } = require("./src-backend/server")
 
-const app = createServer();
+const app = createServer()
 
 app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
-});
+  console.log(`Server running on http://localhost:3000`)
+})
