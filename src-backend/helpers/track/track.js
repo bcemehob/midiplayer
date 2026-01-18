70858b9b32a1ec0a4a9b1663634926bbf5ac47f5
Vadim Semenov <bagum.cemehob@gmail.com>
2026-01-18 20:22:38 +0200

    When party element has party id then don't create new party for them

--- src-backend/helpers/track/track.js
+++ src-backend/helpers/track/track.js
@@ -12,15 +12,24 @@ async function addPartyElement(req, res) {
     const filePath = paths.fullTrackInfoPath(req.params.index)
     const fileContent = await getFile(filePath)
     const trackInfo = Object.assign(new TrackInfo(), JSON.parse(fileContent.toString()))
-    const party = new Party(trackInfo.parties.length, req.body.name, '', req.body.duration)
+    const party = findOrCreateParty(trackInfo, req.body)
     const timelineElement = new TimelineElement(trackInfo.timeline.length, party.id, req.body.start)
     if (!trackInfo.canAddToTimeline(party, timelineElement)) {
         return res.status(400).json({ error: "Timeline element overlaps with existing elements" })
     }
-    trackInfo.parties.push(party)
     trackInfo.timeline.push(timelineElement)
     await save(filePath, JSON.stringify(trackInfo))
     res.json({ result: "OK" })
+}
+
+function findOrCreateParty(trackInfo, requestBody) {
+    const { partyId, name, duration } = requestBody
+    if (!!partyId) {
+        return trackInfo.getParty(partyId)
+    }
+    const party = new Party(trackInfo.parties.length, `Party ${trackInfo.parties.length}`, name, duration)
+    trackInfo.parties.push(party)
+    return party
 }
 
 module.exports = { track, addPartyElement }
